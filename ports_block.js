937705749ce381659967522e6fd6d76574387fbe
michaelmurray-collab <michaelmurray@orenltd.net>
2025-12-04 16:16:53 +0000

    Add files via upload

---
+++ ports_block.js
@@ -0,0 +1,16 @@
+const ports = [
+  {
+    name: "Belgium Transfer Depot",
+    type: "Depot",
+    lat: 51.167356,
+    lng: 5.012402
+  },
+  {
+    name: "Granuband (Amsterdam)",
+    type: "Destination",
+    lat: 52.4224922,
+    lng: 4.7883236
+  }
+];
+
+window.ports = ports;
